db700a49a03f2d6984998ea986a25129c7c3b609
Steven Prescott <steven.prescott@inl.gov>
2021-05-04 09:37:07 -0600

    Added Tests for Prob and Timer variable events

--- UnitTesting_Simulation/SimEngineTests.cs
+++ UnitTesting_Simulation/SimEngineTests.cs
@@ -292,5 +292,55 @@ namespace UnitTesting_Simulation
       //compare the test result and optionally the paths and json if assigned
       Compare(dir, testName, optionsJ);
     }
+
+    [Fact]
+    //Test the timer event using a variable
+    public void TestTimerEvVar()
+    {
+      string testName = GetCurrentMethodName(); //function name must match the name of the test model and saved in the models folder.
+
+      //Setup directory for unit test 
+      string dir = SetupTestDir(testName);
+      //initial options, and optional results to save/test
+      JObject optionsJ = SetupJSON(dir, testName, false, true);
+
+      //Change the default settings as needed for the test seed default set to 0 for testing.
+      optionsJ["inpfile"] = MainTestDir() + modelFolder + testName + ".json";
+      optionsJ["runct"] = 10;
+      optionsJ["debug"] = "basic";
+      JSONRun testRun = new JSONRun(optionsJ.ToString());
+      Assert.True(TestRunSim(testRun));
+
+      //Uncomment to update the validation files after they verified correct
+      //CopyToValidated(dir, testName, optionsJ);
+
+      //compare the test result and optionally the paths and json if assigned
+      Compare(dir, testName, optionsJ);
+    }
+
+    [Fact]
+    //Test the failure rate event using a variable
+    public void TestProbEvVar()
+    {
+      string testName = GetCurrentMethodName(); //function name must match the name of the test model and saved in the models folder.
+
+      //Setup directory for unit test 
+      string dir = SetupTestDir(testName);
+      //initial options, and optional results to save/test
+      JObject optionsJ = SetupJSON(dir, testName);
+
+      //Change the default settings as needed for the test seed default set to 0 for testing.
+      optionsJ["inpfile"] = MainTestDir() + modelFolder + testName + ".json";
+      optionsJ["runct"] = 1000;
+      optionsJ["debug"] = "basic";
+      JSONRun testRun = new JSONRun(optionsJ.ToString());
+      Assert.True(TestRunSim(testRun));
+
+      //Uncomment to update the validation files after they verified correct
+      //CopyToValidated(dir, testName, optionsJ);
+
+      //compare the test result and optionally the paths and json if assigned
+      Compare(dir, testName, optionsJ);
+    }
   }
 }
