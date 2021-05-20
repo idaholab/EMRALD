6b53fb061bf89cc62a9d3f1a18cdd00d96784c1e
michaelzicc <michaelzicc@users.noreply.github.com>
2021-05-19 20:54:40 -0700

    Use Naming Patterns for Variables

--- Emrald_Site/EditForms/VariableEditor.js
+++ Emrald_Site/EditForms/VariableEditor.js
@@ -24,6 +24,40 @@ function setModified(state) {
   isDirty = state;
 }
 
+function loadNamePatterns() {
+    var scope = angular.element(document.querySelector('#variableControllerPanel')).scope();
+    fetch('../resources/DefaultNamingPatterns.json')
+        .then(res => res.json())
+        .then(json => {
+            scope.namingPatterns = json.VariableTypes;
+            updateName();
+            scope.$apply();
+        });
+}
+
+function updateName() {
+    var scope = angular.element(document.querySelector('#variableControllerPanel')).scope();
+    if (!nameIsDefaultValue()) {
+        return;
+    }
+    scope.name = scope.namingPatterns.find(x => x.Type === scope.varType).NamePattern;
+}
+
+function nameIsDefaultValue() {
+    var scope = angular.element(document.querySelector('#variableControllerPanel')).scope();
+    if (scope.name === '') {
+        return true;
+    }
+    result = false;
+    scope.namingPatterns.forEach(defaultName => {
+        if (scope.name === defaultName.NamePattern) {
+            result = true;
+            return;
+        }
+    })
+    return result;
+}
+
 function typeSelection(useDefaultValue = true) {
   var typeVal = document.getElementById("typeVal");
   var scope = angular.element(document.querySelector("#variableControllerPanel")).scope();
@@ -125,16 +159,20 @@ function validateValue() {
 
 function displayErrorMessage(display, message) {
   var scope = angular.element(document.querySelector("#variableControllerPanel")).scope();
-  scope.data.showErrorMessage = display;
-  scope.data.errorMessage = message;
-  scope.$apply(() => {});
-  var btn = parent.document.getElementById("btn_OK");
-  btn.disabled = display;
+    if (scope.data.showErrorMessage !== display) {
+
+        scope.data.showErrorMessage = display;
+        scope.data.errorMessage = message;
+       // scope.$apply(() => { });
+        var btn = parent.document.getElementById("btn_OK");
+        btn.disabled = display;
+    }
 }
 
 
 var variableData = null;
 function OnLoad(dataObj) {
+  loadNamePatterns();
   variableData = dataObj || {};
   var scope = angular.element(document.querySelector("#variableControllerPanel")).scope();
 
@@ -402,13 +440,13 @@ function handleVarScopeChanged(newV, oldV) {
       break;
   }
  
-  
   somethingChanged();
 }
 
 var variableModule = angular.module("variableModule", []);
 variableModule.controller("variableController", ["$scope", function ($scope) {
   $scope.name = "";
+  $scope.namingPatterns = [];
   $scope.desc = "";
 
   $scope.docTypes = [
@@ -450,6 +488,7 @@ variableModule.controller("variableController", ["$scope", function ($scope) {
 
   $scope.$watch("name", function (newV, oldV) { if (newV !== oldV) somethingChanged(); });
   $scope.$watch("desc", function (newV, oldV) { if (newV !== oldV) somethingChanged(); });
+  $scope.$watch("varType", function (newV, oldV) { if (newV !== oldV) { somethingChanged(); updateName(); } });
   $scope.$watch("varScope", function (newV, oldV) { if (newV !== oldV) handleVarScopeChanged(newV, oldV); });
   $scope.$watch("data.value", function (newV, oldV) { if (newV !== oldV) validateValue(); });
   $scope.$watch("data.sim3DId", function (newV, oldV) { if (newV !== oldV) somethingChanged(); });
