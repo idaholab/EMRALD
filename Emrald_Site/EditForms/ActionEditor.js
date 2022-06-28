47c16ab59048b4b1dea997c968297ce5ffb164ef
Daniel Nevius <dmnevius@ncsu.edu>
2022-06-28 14:26:47 -0600

    Added UI code for ra code/form

--- Emrald_Site/EditForms/ActionEditor.js
+++ Emrald_Site/EditForms/ActionEditor.js
@@ -302,6 +302,7 @@ function OnLoad(dataObj) {
                 break;
             case 'atRunExtApp':
                 actTypeEl.selectedIndex = 3;
+                scope.data.raType = actionData.raType || 'template';
                 scope.data.raPreCode = actionData.makeInputFileCode;
                 scope.data.raLocation = actionData.exePath;
                 scope.data.raPostCode = actionData.processOutputFileCode;
@@ -443,6 +444,7 @@ function GetDataObject() {
             }
             break;
         case 'atRunExtApp':
+            dataObj.raType = scope.data.raType;
             dataObj.makeInputFileCode = scope.data.raPreCode;
             dataObj.exePath = scope.data.raLocation;
             dataObj.processOutputFileCode = scope.data.raPostCode;
