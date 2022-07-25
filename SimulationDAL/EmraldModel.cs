10c8f533211ef2ba9eada3521fb186c8016b714d
Steven Prescott <steven.prescott@inl.gov>
2022-07-25 09:11:04 -0600

    Fixed timespan overflow and added a reset to IDs when loading a new model.

--- SimulationDAL/EmraldModel.cs
+++ SimulationDAL/EmraldModel.cs
@@ -121,6 +121,7 @@ namespace SimulationDAL
 
     public bool DeserializeJSON(string jsonModel, string modelPath) 
     {
+      SingleNextIDs.Instance.Reset();
       this.rootPath = modelPath;
       dynamic jsonObj = JsonConvert.DeserializeObject(jsonModel); 
       return DeserializeDerived(jsonObj, true, this, false);
