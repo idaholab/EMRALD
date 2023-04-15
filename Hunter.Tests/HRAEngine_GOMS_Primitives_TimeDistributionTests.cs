ac673c987a39ab5adf0b44b0f499c1e4eb3b987a
Roger Lew <rogerlew@gmail.com>
2023-04-15 07:50:02 -0700

    Create HRAEngine_GOMS_Primitives_TimeDistributionTests.cs
        Added a new test case for the time distribution of GOMS primitives in the HRAEngine class.
        The test generates a table of mean time and time standard deviations for each primitive using the MathNet.Numerics.Statistics library.
        The test iterates over each primitive in the HRAEngine and evaluates it 10,000 times to gather enough data for accurate statistics.
        The test verifies that the mean and standard deviation times are greater than 0 using NUnit's Assert.That method.
        The test outputs the results to the console using TestContext.Out.WriteLine.
        The purpose of this test is to ensure the accuracy and reliability of the HRAEngine's timing functionality.

---
+++ Hunter.Tests/HRAEngine_GOMS_Primitives_TimeDistributionTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using MathNet.Numerics.Statistics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunter.Tests
+{
+    [TestFixture]
+    public class HRAEngine_GOMS_Primitives_TimeDistributionTests
+    {
+        bool success = false;
+        int count = 10000;
+        List<double> times;
+
+        /// <summary>
+        /// Generates table of mean time and time stdeves
+        /// </summary>
+        [Test]
+        public void GOMS_Primitives_TimeDistribution_Test()
+        {
+            HRAEngine engine = new HRAEngine();
+            engine.RepeatMode = false;
+
+            foreach (HRAEngine.Primitive primitive in engine)
+            {
+                times = new List<double>();
+
+                var primitiveIds = new List<string> { primitive.Id };
+
+                for (int i = 0; i < count; i++)
+                {
+                    times.Add(engine.Evaluate(primitiveIds, ref success));
+                }
+
+                double mean = Math.Round(times.Average(), 2);
+                double stdev = Math.Round(times.StandardDeviation(), 2);
+
+                Assert.That(mean > 0, Is.True);
+                Assert.That(stdev > 0, Is.True);
+
+                TestContext.Out.WriteLine($"Primitive ID: {primitive.Id } \tMean Time: {mean} s \tTime Stdev: {stdev} s");
+            }
+        }
+    }
+}
