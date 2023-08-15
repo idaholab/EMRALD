c1fe7911278a1d67d0af9f6f0e461a343bb2f2ed
jaredNielsonINL <48254679+jaredNielson33@users.noreply.github.com>
2023-08-15 12:49:11 -0600

    Connect variable editor to json
    Make it so changes to the monitor checkboxes are reflected on the json file when downloading.

--- Emrald_Site/EditForms/VariableEditor.js
+++ Emrald_Site/EditForms/VariableEditor.js
@@ -189,6 +189,11 @@ function OnLoad(dataObj) {
     scope.dbID = variableData.dbID;
     scope.name = variableData.name;
     scope.desc = variableData.desc;
+
+    scope.data.canMonitor = variableData.canMonitor;
+    scope.data.monitorInSim = variableData.monitorInSim;
+    scope.data.cumulativeStats = variableData.cumulativeStats;
+
     if (typeof variableData.resetOnRuns !== "undefined") {
       scope.data.resetOnRuns = variableData.resetOnRuns;
     }
@@ -310,6 +315,11 @@ function GetDataObject() {
   dataObj.name = scope.name;
   dataObj.desc = scope.desc;
   dataObj.varScope = scope.varScope.value;
+
+  dataObj.canMonitor = scope.data.canMonitor;
+  dataObj.monitorInSim = scope.data.monitorInSim;
+  dataObj.cumulativeStats = scope.data.cumulativeStats;
+  
   if (dataObj.varScope === "gtDocLink") {
     dataObj.value = scope.data.value;
     dataObj.docLink = scope.data.varLink;
@@ -473,9 +483,10 @@ variableModule.controller("variableController", ["$scope", function ($scope) {
     docType: $scope.docTypes[0],
     docPath: "",
     varLink: "",
-    cumulativeStats: false,
-    monitorInSim: false,
+
     canMonitor: false,
+    monitorInSim: false,
+    cumulativeStats: false,
     
     useRegExLine: false,
     regExLine: -1,
@@ -508,7 +519,11 @@ variableModule.controller("variableController", ["$scope", function ($scope) {
   $scope.$watch("data.value", function (newV, oldV) { if (newV !== oldV) validateValue(); });
   $scope.$watch("data.sim3DId", function (newV, oldV) { if (newV !== oldV) somethingChanged(); });
   $scope.$watch("data.docType", function (newV, oldV) { if (newV !== oldV) handleDocTypeChange(); });
-  $scope.$watch("data.canMonitor", function (newV, oldV) { if (newV !== true) $scope.data.monitorInSim = false; $scope.data.cumulativeStats = false; });
+  $scope.$watch("data.canMonitor", function (newV) { 
+    if (newV === false) { 
+    $scope.data.monitorInSim = false; 
+    $scope.data.cumulativeStats = false; 
+  }});
 
   $scope.accrualScopeSelected= false;
   $scope.extSimVariableScopeSelected = false;
