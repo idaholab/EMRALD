f9aad287524e6991b333090d22a5b403484e6498
jaredNielsonINL <48254679+jaredNielson33@users.noreply.github.com>
2024-05-09 12:05:37 -0600

    Add comments and validation check to custom form template component

--- Emrald-UI/src/components/forms/ActionForm/FormFieldsByType/RunApplication/CustomForms/useCustomForm.tsx
+++ Emrald-UI/src/components/forms/ActionForm/FormFieldsByType/RunApplication/CustomForms/useCustomForm.tsx
@@ -1,26 +1,78 @@
+import { useMemo, useState } from "react";
 import { useActionContext } from "../../../../../../contexts/ActionContext";
 import { useDiagramContext } from "../../../../../../contexts/DiagramContext";
 import { useEventContext } from "../../../../../../contexts/EventContext";
 import { useLogicNodeContext } from "../../../../../../contexts/LogicNodeContext";
 import { useStateContext } from "../../../../../../contexts/StateContext";
 import { useVariableContext } from "../../../../../../contexts/VariableContext";
+import { useActionFormContext } from "../../../ActionFormContext";
 
 export function useCustomForm() {
   const { diagramList: { value: diagrams } } = useDiagramContext();
-  const {logicNodeList: { value: logicNodes }} = useLogicNodeContext();
+  const { logicNodeList: { value: logicNodes }} = useLogicNodeContext();
   const { actionsList: { value: actions }} = useActionContext();
   const { eventsList: { value: events } } = useEventContext();
   const { statesList: { value: states } } = useStateContext();
   const { variableList: { value: variables }} = useVariableContext();
+  const { formData, codeVariables, setFormData, setExePath, setMakeInputFileCode, setProcessOutputFileCode, setCodeVariables } = useActionFormContext();
+  const [preCodeUsed, setPreCodeUsed] = useState(false);
+  const [postCodeUsed, setPostCodeUsed] = useState(false);
+  const [exePathUsed, setExePathUsed] = useState(false);
+  const [codeVariablesUsed, setCodeVariablesUsed] = useState(false);
   // ... get data from other contexts
+
+  const resetUseCode = () => {
+    setMakeInputFileCode('');
+    setProcessOutputFileCode('');
+    setExePath('');
+    setCodeVariables(codeVariables);
+  }
+
+  const isValid = useMemo(() => {
+    if (!preCodeUsed || !postCodeUsed || !exePathUsed || !codeVariablesUsed) {
+      resetUseCode();
+    }
+    return preCodeUsed && postCodeUsed && exePathUsed && codeVariablesUsed;
+  }, [preCodeUsed, postCodeUsed, exePathUsed, codeVariablesUsed]);
+
+  const ReturnPreCode = (preCode: string) => {
+    setMakeInputFileCode(preCode);
+    setPreCodeUsed(true);
+  }
+
+  const ReturnPostCode = (postCode: string) => {
+    setProcessOutputFileCode(postCode);
+    setPostCodeUsed(true);
+  }
+
+  const ReturnExePath = (path: string) => {
+    setExePath(path);
+    setExePathUsed(true);
+  }
+
+  const ReturnUsedVariables = (variableName: string) => {
+    if (!codeVariables.includes(variableName)) {
+      setCodeVariables([...codeVariables, variableName]);
+    } else {
+      setCodeVariables(codeVariables.filter((item) => item !== variableName));
+    }
+    setCodeVariablesUsed(true);
+  }
 
 
   return {
+    formData,
+    isValid,
     diagrams,
     logicNodes,
     actions,
     events,
     states,
     variables,
+    setFormData,
+    ReturnPreCode,
+    ReturnPostCode,
+    ReturnExePath,
+    ReturnUsedVariables
   };
 }
