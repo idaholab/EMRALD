0d2b76d7b3de362246eb4e84ddf2287200dc091e
Steven Prescott <steven.prescott@inl.gov>
2024-05-14 11:04:58 -0600

    Save UpdateModel to modifiy a specified model. Working on validating using the schema

--- Emrald-UI/src/utils/Upgrades/upgrade.ts
+++ Emrald-UI/src/utils/Upgrades/upgrade.ts
@@ -3,7 +3,6 @@ import { v4 as uuidv4 } from 'uuid';
 import { EMRALD_Model } from "../../types/EMRALD_Model";
 
 
-
  export function upgradeModel (toVersion: number, emraldData : string): EMRALD_Model | null{
     const upgradeModel = new Upgrade(emraldData);
     upgradeModel.upgradeGiveID(3.0, uuidv4); // upgrade to version 3.0 true;
@@ -15,75 +14,8 @@ import { EMRALD_Model } from "../../types/EMRALD_Model";
     }
 }
 
-
-    // upgrade(toVersion : number, resetIDs : boolean = true) : boolean
-    // {
-    //     const badModel = "Invalid EMRALD model format ";
-    //     this._newModelTxt = this._oldModelTxt;
-        
-    //     //go through each upgrade version 
-    //     if(this._version < 1.2){
-    //         var upgraded = UpgradeV1_x(this._newModelTxt);
-    //         this._newModelTxt = upgraded.newModel;
-    //         this._newModel = JSON.parse(upgraded.newModel); 
-
-    //         if(upgraded.errors.length > 0){
-    //             this._errors = badModel + "v1.2 -" + upgraded.errors;
-    //             return false;
-    //         }
-    //         this._version = 1.2;     
-    //     } 
-        
-    //     if((this._version < 2.4) && (toVersion >= 2.4)){
-    //         var upgraded = UpgradeV2_4(this._newModelTxt);
-    //         this._newModelTxt = upgraded.newModel;
-    //         this._newModel = JSON.parse(upgraded.newModel);  
-
-    //         if(upgraded.errors.length > 0){
-    //             this._errors = badModel + "v2.4 -" + upgraded.errors;
-    //             return false;
-    //         }
-    //         this._version = 2.4; 
-    //     } 
-
-    //     if((this._version < 3.0) && (toVersion >= 3.0)){
-    //         var upgraded = UpgradeV3_0(this._newModelTxt);
-    //         this._newModelTxt = upgraded.newModel;
-    //         this._newModel = JSON.parse(upgraded.newModel);
-            
-    //         if(upgraded.errors.length > 0){
-    //             this._errors = badModel + "v2.4 -" + upgraded.errors;
-    //             return false;
-    //         }
-    //         this._version = 3.0; 
-    //     } 
-        
-        
-
-    //     if(resetIDs)
-    //     {
-    //         this._newModel?.ActionList.forEach(element => {
-    //             element.id = uuidv4();
-    //         });
-    //         this._newModel?.DiagramList.forEach(element => {
-    //             element.id = uuidv4();
-    //         });
-    //         this._newModel?.EventList.forEach(element => {
-    //             element.id = uuidv4();
-    //         });
-    //         this._newModel?.ExtSimList.forEach(element => {
-    //             element.id = uuidv4();
-    //         });
-    //         this._newModel?.LogicNodeList.forEach(element => {
-    //             element.id = uuidv4();
-    //         });
-    //         this._newModel?.StateList.forEach(element => {
-    //             element.id = uuidv4();
-    //         });
-    //         this._newModel?.VariableList.forEach(element => {
-    //             element.id = uuidv4();
-    //         });
-    //     }
-
-    //     return true;
-    // }
+export async function validateModel (eModel: EMRALD_Model): Promise<string> {
+    const upgradeModel = new Upgrade("");
+    await upgradeModel.validateModel(eModel); 
+    return upgradeModel.errorsStr;
+}
--- Emrald-UI/src/utils/Upgrades/upgradeGiveID.ts
+++ Emrald-UI/src/utils/Upgrades/upgradeGiveID.ts
@@ -2,7 +2,11 @@ import { EMRALD_Model } from "./v3_0/AllModelInterfacesV3_0";
 import { UpgradeV1_x } from "./v1_x/UpgradeV1_x"
 import { UpgradeV2_4 } from "./v2_4/UpgradeV2_4"
 import { UpgradeV3_0 } from "./v3_0/UpgradeV3_0"
-import {UpgradeReturn} from "./v1_x/UpgradeV1_x"
+import { UpgradeReturn } from "./v1_x/UpgradeV1_x"
+import { Validator } from "jsonschema";
+
+
+//import { Validator } from "../../../node_modules/jsonschema/lib/index";
 //import { v4 as uuidv4 } from 'uuid';
 
 // interface UpgradeResult {
@@ -22,12 +26,17 @@ export class Upgrade
 
     constructor(modelTxt: string) 
     {
+        this._emraldVersion = 0.0;
         this._oldModelTxt = modelTxt;
-        const modelObj = JSON.parse(modelTxt);
-        //using emraldVersion for now
-        this._emraldVersion = ('schemaVerson' in modelObj) ? modelObj.emraldVersion : null;
-        if (this._emraldVersion == null) //if no emraldVersion use old version tag.
-            this._emraldVersion = ('verson' in modelObj) ? modelObj.version : 0.0;
+        let modelObj = null;
+        if(modelTxt != ""){
+            modelObj = JSON.parse(modelTxt);
+        
+            //using emraldVersion for now
+            this._emraldVersion = ('schemaVerson' in modelObj) ? modelObj.emraldVersion : null;
+            if (this._emraldVersion == null) //if no emraldVersion use old version tag.
+                this._emraldVersion = ('verson' in modelObj) ? modelObj.version : 0.0;
+        }
         this._newModelTxt = "";
         this._newModel = undefined;
         this._emraldVersion = 0.0;
@@ -91,6 +100,38 @@ export class Upgrade
     }
 
     get errorsStr (): string {
+        return this._errors;
+    }
+
+    // 
+    
+    async validateModel(model: EMRALD_Model): Promise<string> {
+        this._errors = "";
+        const schemaPath = './src/utils/Upgrades/v3_0/EMRALD_JsonSchemaV3_0.json';
+    
+        try {
+            const response = await fetch(schemaPath);
+            if (!response.ok) {
+                throw new Error("Failed to fetch schema text");
+            }
+            
+            const schemaTxt = await response.text();
+            const schema = JSON.parse(schemaTxt);
+            const validator = new Validator();
+            const validationResult = validator.validate(model, schema);
+    
+            if (!validationResult.valid) {
+                validationResult.errors.forEach(error => {
+                    if (this._errors !== "") {
+                        this._errors += "\n";
+                    }
+                    this._errors = this._errors + error.instance + " - " + error.message + " : " + JSON.stringify(error.argument);
+                });
+            }
+        } catch (error) {
+            this._errors = error.message;
+        }
+    
         return this._errors;
     }
 }
