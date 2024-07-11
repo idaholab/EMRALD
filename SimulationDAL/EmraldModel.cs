1a6d6b33bbb92c893fb1ca0c96c5b3542e9fa400
Steven Prescott <steven.prescott@inl.gov>
2024-07-11 12:29:37 -0600

    Past current version fix

--- SimulationDAL/EmraldModel.cs
+++ SimulationDAL/EmraldModel.cs
@@ -139,6 +139,11 @@ namespace SimulationDAL
           throw new Exception("Failed to Upgrade old model to v" + SCHEMA_VERSION + ex.Message);
         }
       }
+      else if(jsonObj.emraldVersion > SCHEMA_VERSION)
+      {
+        throw new Exception("EMRALD solver version to old to solve this model. Upgrade to v" + jsonObj.emraldVersion);
+      }
+     
       return jsonModel;
     }
 
