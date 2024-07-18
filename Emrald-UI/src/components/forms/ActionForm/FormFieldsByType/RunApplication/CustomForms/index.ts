a749e8c4ac814c764819c457ab0252c6c65b19cd
John Stewart <john.stewart@inl.org>
2024-07-18 15:54:01 -0600

    Allow Maap custom action

--- Emrald-UI/src/components/forms/ActionForm/FormFieldsByType/RunApplication/CustomForms/index.ts
+++ Emrald-UI/src/components/forms/ActionForm/FormFieldsByType/RunApplication/CustomForms/index.ts
@@ -2,8 +2,7 @@
 // New custom forms must be imported and exported here to be accessible in the dropdown menu of use custom application.
 
 import CustomFormTemplate from './CustomFormTemplate';
-// import MAAP from './MAAP/maap'; // uncomment this when Peggy is complete
+import MAAP from './MAAP/maap'; // uncomment this when Peggy is complete
 
 
-// export { CustomFormTemplate, MAAP } // TODO add MAAP back in when Peggy is complete
-export { CustomFormTemplate }
+export { CustomFormTemplate, MAAP } // TODO add MAAP back in when Peggy is complete
