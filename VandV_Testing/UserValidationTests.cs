1ae16495801051f517921c2ec3e4d994edbb7fe3
pressr-inl <Steven.Prescott@inl.gov>
2025-02-21 12:03:32 -0700

    Testing Info update

--- VandV_Testing/UserValidationTests.cs
+++ VandV_Testing/UserValidationTests.cs
@@ -41,51 +41,41 @@ namespace UserTesting
 
 
     [Fact]// (Skip = "This is a template only. Do not skip actual tests.")]
-        public void Template_ValidationCase_Test()
-        {
-            string testName = GetCurrentMethodName(); //function name must match the name of the test model and saved in the models folder.
-
-            //Setup directory for unit test 
-            string dir = SetupTestDir(testName);
-            //initial options, and optional results to save/test
-            JObject optionsJ = SetupJSON(dir, testName);
-
-            //Change the default settings as needed for the test seed default set to 0 for testing.
-            optionsJ["inpfile"] = MainTestDir() + ModelFolder() + testName + ".emrald";
-            optionsJ["runct"] = 1;
-            optionsJ["debug"] = "basic";
-            JSONRun testRun = new JSONRun(optionsJ.ToString());
-            Assert.True(TestRunSim(testRun));
-
-            //Uncomment to update the validation files after they verified correct
-            //CopyToValidated(dir, testName, optionsJ);
-
-            //compare the test result and optionally the paths and json if assigned
-            Compare(dir, testName, optionsJ);
-        }
-    }
-
-      [Fact]// (Skip = "This is a template only. Do not skip actual tests.")]
-    public void Template_ValidationCase_Test2()
+    public void Template_ValidationCase_Test() //Name the test 
     {
-      string testName = GetCurrentMethodName(); //function name must match the name of the test model and saved in the models folder.
+      //Description 
+      //[describe the test]
 
-      //Setup directory for unit test 
+      //Validation Document
+      //[name of validation document in TestingFiles/UserValidationDocs see Template_ValidationCase_Doc.docx]
+
+      //////////////////////////////
+      ///Don't change the following
+      //////////////////////////////
+      string testName = GetCurrentMethodName(); //function name must match the name of the test model and saved in the models folder.
+      //Sets up directory for unit test 
       string dir = SetupTestDir(testName);
       //initial options, and optional results to save/test
       JObject optionsJ = SetupJSON(dir, testName);
-
-      //Change the default settings as needed for the test seed default set to 0 for testing.
       optionsJ["inpfile"] = MainTestDir() + ModelFolder() + testName + ".emrald";
+      /////////////////////////////////
+
+      /////////////
+      ///Use the optionsJ to set the model run settings needed 
+      ///please only use the number of runs needed to validate the test so testing can get done quickly
+      ///Change the default settings as needed for the test seed default set to 0 for testing.
       optionsJ["runct"] = 1;
       optionsJ["debug"] = "basic";
       JSONRun testRun = new JSONRun(optionsJ.ToString());
       Assert.True(TestRunSim(testRun));
 
-      //Uncomment to update the validation files after they verified correct
+
+      //If verifying against a different results file use this code, otherwise delete and add the verification asserts 
+
+      //Uncomment on the first run then verify the result file is correct only ever uncomment again if updating the validation results and they are verified
       //CopyToValidated(dir, testName, optionsJ);
 
-      //compare the test result and optionally the paths and json if assigned
+      //compare the test result and optionally the paths and json if assigned, causes an assert if not equiovolent
       Compare(dir, testName, optionsJ);
     }
   }
