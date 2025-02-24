d3730918768035e93d7864075e81c1b603dd9617
Daniel Nevius <daniel.nevius@inl.gov>
2025-02-24 08:03:06 -0700

    Added reset on runs option to accrual variables

--- Emrald-UI/src/components/forms/VariableForm/FormFieldsByType/AccrualFields.tsx
+++ Emrald-UI/src/components/forms/VariableForm/FormFieldsByType/AccrualFields.tsx
@@ -1,10 +1,24 @@
+import { Checkbox, FormControlLabel } from '@mui/material';
 import StateDropTarget from '../../../drag-and-drop/StateDroppable';
+import { useVariableFormContext } from '../VariableFormContext';
 
 const AccrualFields = () => {
+  const { resetOnRuns, setResetOnRuns } = useVariableFormContext();
+
   return (
     <>
       State Accrual Variables
       <StateDropTarget />
+      <FormControlLabel
+        label="Reset to initial value for every simulation run"
+        value={resetOnRuns}
+        control={
+          <Checkbox
+            checked={resetOnRuns}
+            onChange={(e) => setResetOnRuns(e.target.checked)}
+          ></Checkbox>
+        }
+      ></FormControlLabel>
     </>
   );
 };
