2d6a743dc5e203b480e19e4d3a5c47424dac2e41
Daniel Nevius <daniel.nevius@inl.gov>
2025-02-28 11:09:25 -0700

    Ext sim event tests

--- Emrald-UI/src/tests/official/forms/EventForm/FormFieldsByType/ComponentLogic.test.tsx
+++ Emrald-UI/src/tests/official/forms/EventForm/FormFieldsByType/ComponentLogic.test.tsx
@@ -1,111 +1,127 @@
-import { describe, expect, test } from "vitest";
-import userEvent from "@testing-library/user-event";
+import { describe, expect, test } from 'vitest';
+import userEvent from '@testing-library/user-event';
 import { findByRole, screen } from '@testing-library/react';
-import { getEvent, render, updateModel } from "../../../../test-utils";
-import EventContextProvider from "../../../../../contexts/EventContext";
-import EventFormContextProvider from "../../../../../components/forms/EventForm/EventFormContext";
-import EventForm from "../../../../../components/forms/EventForm/EventForm";
+import { getEvent, render, updateModel } from '../../../../test-utils';
+import EventContextProvider from '../../../../../contexts/EventContext';
+import EventFormContextProvider from '../../../../../components/forms/EventForm/EventFormContext';
+import EventForm from '../../../../../components/forms/EventForm/EventForm';
 import expected from './ComponentLogic.expected.json';
 
 describe('Component Logic Events', () => {
-    test('sets default values', async () => {
-        const name = 'sets default values';
-        render(<EventContextProvider>
-            <EventFormContextProvider>
-                <EventForm eventData={{
-                    objType: 'Event',
-                    name,
-                    desc: '',
-                    mainItem: true,
-                    evType: 'etComponentLogic'
-                }}></EventForm>
-            </EventFormContextProvider>
-        </EventContextProvider>);
-        const user = userEvent.setup();
+  test('sets default values', async () => {
+    const name = 'sets default values';
+    render(
+      <EventContextProvider>
+        <EventFormContextProvider>
+          <EventForm
+            eventData={{
+              objType: 'Event',
+              name,
+              desc: '',
+              mainItem: true,
+              evType: 'etComponentLogic',
+            }}
+          ></EventForm>
+        </EventFormContextProvider>
+      </EventContextProvider>,
+    );
+    const user = userEvent.setup();
 
-        await user.click(await screen.findByText('Save'));
-        expect(getEvent(name)).toEqual(expected[name]);
+    await user.click(await screen.findByText('Save'));
+    expect(getEvent(name)).toEqual(expected[name]);
+  });
+
+  test('sets on success', async () => {
+    const name = 'sets on success';
+    render(
+      <EventContextProvider>
+        <EventFormContextProvider>
+          <EventForm
+            eventData={{
+              objType: 'Event',
+              name,
+              desc: '',
+              mainItem: true,
+              evType: 'etComponentLogic',
+            }}
+          ></EventForm>
+        </EventFormContextProvider>
+      </EventContextProvider>,
+    );
+    const user = userEvent.setup();
+
+    // Select "Success Tree"
+    await user.click(await screen.findByLabelText('Success Tree'));
+
+    await user.click(await screen.findByText('Save'));
+    expect(getEvent(name)).toEqual(expected[name]);
+  });
+
+  test('sets trigger on false', async () => {
+    const name = 'sets trigger on false';
+    render(
+      <EventContextProvider>
+        <EventFormContextProvider>
+          <EventForm
+            eventData={{
+              objType: 'Event',
+              name,
+              desc: '',
+              mainItem: true,
+              evType: 'etComponentLogic',
+            }}
+          ></EventForm>
+        </EventFormContextProvider>
+      </EventContextProvider>,
+    );
+    const user = userEvent.setup();
+
+    // Select "Trigger on False"
+    await user.click(await screen.findByLabelText('Trigger on False'));
+
+    await user.click(await screen.findByText('Save'));
+    expect(getEvent(name)).toEqual(expected[name]);
+  });
+
+  test('sets logic top', async () => {
+    const name = 'sets logic top';
+    render(
+      <EventContextProvider>
+        <EventFormContextProvider>
+          <EventForm
+            eventData={{
+              objType: 'Event',
+              name,
+              desc: '',
+              mainItem: true,
+              evType: 'etComponentLogic',
+            }}
+          ></EventForm>
+        </EventFormContextProvider>
+      </EventContextProvider>,
+    );
+    const user = userEvent.setup();
+
+    // Add a logic node to the model
+    await user.click(await screen.findByText('Save'));
+    updateModel((model) => {
+      model.LogicNodeList.push({
+        objType: 'LogicNode',
+        name: 'Test Logic Node',
+        desc: '',
+        gateType: 'gtAnd',
+        compChildren: [],
+        gateChildren: [],
+        isRoot: true,
+      });
+      return model;
     });
 
-    test('sets on success', async () => {
-        const name = 'sets on success';
-        render(<EventContextProvider>
-            <EventFormContextProvider>
-                <EventForm eventData={{
-                    objType: 'Event',
-                    name,
-                    desc: '',
-                    mainItem: true,
-                    evType: 'etComponentLogic'
-                }}></EventForm>
-            </EventFormContextProvider>
-        </EventContextProvider>);
-        const user = userEvent.setup();
+    // Select the logic node
+    await user.click(await findByRole(await screen.findByLabelText('LogicTop'), 'combobox'));
+    await user.click(await screen.findByRole('option', { name: 'Test Logic Node' }));
 
-        // Select "Success Tree"
-        await user.click(await screen.findByLabelText('Success Tree'));
-
-        await user.click(await screen.findByText('Save'));
-        expect(getEvent(name)).toEqual(expected[name]);
-    });
-
-    test('sets trigger on false', async () => {
-        const name = 'sets trigger on false';
-        render(<EventContextProvider>
-            <EventFormContextProvider>
-                <EventForm eventData={{
-                    objType: 'Event',
-                    name,
-                    desc: '',
-                    mainItem: true,
-                    evType: 'etComponentLogic'
-                }}></EventForm>
-            </EventFormContextProvider>
-        </EventContextProvider>);
-        const user = userEvent.setup();
-
-        // Select "Trigger on False"
-        await user.click(await screen.findByLabelText('Trigger on False'));
-
-        await user.click(await screen.findByText('Save'));
-        expect(getEvent(name)).toEqual(expected[name]);
-    });
-
-    test('sets logic top', async () => {
-        const name = 'sets logic top';
-        render(<EventContextProvider>
-            <EventFormContextProvider>
-                <EventForm eventData={{
-                    objType: 'Event',
-                    name,
-                    desc: '',
-                    mainItem: true,
-                    evType: 'etComponentLogic'
-                }}></EventForm>
-            </EventFormContextProvider>
-        </EventContextProvider>);
-        const user = userEvent.setup();
-
-        // Add a logic node to the model
-        await user.click(await screen.findByText('Save'));
-        updateModel((model) => {
-            model.LogicNodeList.push({
-                objType: 'LogicNode',
-                name: 'Test Logic Node',
-                desc: '',
-                gateType: 'gtAnd',
-                compChildren: [],
-                gateChildren: [],
-                isRoot: true,
-            });
-            return model;
-        });
-
-        // Select the logic node
-        await user.click(await findByRole(await screen.findByLabelText('LogicTop'), 'combobox'));
-        await user.click(await screen.findByRole('option', { name: 'Test Logic Node' }));
-
-        await user.click(await screen.findByText('Save'));
-        expect(getEvent(name)).toEqual(expected[name]);
-    });
-})
+    await user.click(await screen.findByText('Save'));
+    expect(getEvent(name)).toEqual(expected[name]);
+  });
+});
---
+++ Emrald-UI/src/tests/official/forms/EventForm/FormFieldsByType/ExtSim.test.tsx
@@ -0,0 +1,94 @@
+import { describe, expect, test } from 'vitest';
+import { getEvent, render, updateModel } from '../../../../test-utils';
+import EventContextProvider from '../../../../../contexts/EventContext';
+import EventFormContextProvider from '../../../../../components/forms/EventForm/EventFormContext';
+import EventForm from '../../../../../components/forms/EventForm/EventForm';
+import userEvent from '@testing-library/user-event';
+import { findByRole, screen } from '@testing-library/react';
+import expected from './ExtSim.expected.json';
+
+describe('ExtSim Events', () => {
+  test('selects event type', async () => {
+    const name = 'selects event type';
+    render(
+      <EventContextProvider>
+        <EventFormContextProvider>
+          <EventForm
+            eventData={{
+              objType: 'Event',
+              name,
+              desc: '',
+              mainItem: true,
+              evType: 'et3dSimEv',
+            }}
+          ></EventForm>
+        </EventFormContextProvider>
+      </EventContextProvider>,
+    );
+    const user = userEvent.setup();
+
+    // Select an event type
+    await user.click(
+      await findByRole(await screen.findByLabelText('External Event Type'), 'combobox'),
+    );
+    await user.click(await screen.findByRole('option', { name: 'Simulation End' }));
+
+    await user.click(await screen.findByText('Save'));
+    expect(getEvent(name)).toEqual(expected[name]);
+  });
+
+  test('sets variable change', async () => {
+    const name = 'sets variable change';
+    render(
+      <EventContextProvider>
+        <EventFormContextProvider>
+          <EventForm
+            eventData={{
+              objType: 'Event',
+              name,
+              desc: '',
+              mainItem: true,
+              evType: 'et3dSimEv',
+            }}
+          ></EventForm>
+        </EventFormContextProvider>
+      </EventContextProvider>,
+    );
+    const user = userEvent.setup();
+
+    // Select variable change type
+    await user.click(
+      await findByRole(await screen.findByLabelText('External Event Type'), 'combobox'),
+    );
+    await user.click(await screen.findByRole('option', { name: 'Variable Change' }));
+
+    // Add an external sim variable to the model
+    await user.click(await screen.findByText('Save'));
+    updateModel((model) => {
+      model.VariableList.push({
+        objType: 'Variable',
+        name: 'Test ExtSim Variable',
+        varScope: 'gt3DSim',
+        value: '',
+        type: 'string',
+      });
+      return model;
+    });
+
+    // Select the ext sim variable
+    await user.click(
+      await findByRole(await screen.findByLabelText('External Sim Variable'), 'combobox'),
+    );
+    await user.click(await screen.findByRole('option', { name: 'Test ExtSim Variable' }));
+
+    // Type in code
+    // TODO: The Monaco editor apparently can't be tested using JSDOM, so the "code" property is currently untested
+    //await user.type(await screen.findByRole('code'), 'return "";');
+
+    // Check the ext sim variable as being used in the code
+    await user.click(await screen.findByLabelText('Test ExtSim Variable'));
+
+    await user.click(await screen.findByText('Save'));
+    expect(getEvent(name)).toEqual(expected[name]);
+  });
+});
