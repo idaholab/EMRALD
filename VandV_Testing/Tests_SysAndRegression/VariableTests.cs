992b0ce974f43f588d1e003ead275ac943eb85c4
pressr-inl <Steven.Prescott@inl.gov>
2025-06-26 17:09:52 -0600

    Added test for variable in XPath

--- VandV_Testing/Tests_SysAndRegression/VariableTests.cs
+++ VandV_Testing/Tests_SysAndRegression/VariableTests.cs
@@ -174,7 +174,40 @@ namespace SysAndRegressionTesting
       Compare(dir, testName, optionsJ);
     }
 
-    
+    [Fact]
+    [Description("Test XML document Link variable that uses a variable in the XPath")]
+    public void Dynamic_XML_Var_Example_with_CurTime()
+    {
+      //FYI - model must have the XML document using relative path to ..\..\..\UnitTesting_Simulation\TestingFiles\Other\
+      //Save the XML document in that folder
+      string testName = GetCurrentMethodName(); //function name must match the name of the test model and saved in the models folder.
+
+      //Setup directory for unit test 
+      string dir = SetupTestDir(testName);
+      //initial options, and optional results to save/test
+      JObject optionsJ = SetupJSON(dir, testName, false);
+
+      SimulationEngine.Options_cur options = optionsJ.ToObject<SimulationEngine.Options_cur>();
+      //Change the default settings as needed for the test seed default set to 0 for testing.
+      options.inpfile = MainTestDir() + ModelFolder() + testName + ".emrald";
+      options.runct = 1;
+      options.runtime = "365.00:00:00";
+
+      string exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+      //todo set variables to watch initial doc link var value and then after it is set.
+      //options.variables = new List<string>() { "State1", "state2" };
+      JSONRun testRun = new JSONRun(options);
+      Assert.True(TestRunSim(testRun));
+
+      //Uncomment to update the validation files after they verified correct
+      CopyToValidated(dir, testName, optionsJ);
+
+      //compare the test result and optionally the paths and json if assigned
+      Compare(dir, testName, optionsJ);
+    }
+
+
 
     [Fact]
     [Description("Test JSON document Link variable to make sure it is reading an writing correctly")]
