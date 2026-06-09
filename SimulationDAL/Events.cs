15cc7a4f45e8a84c99d9c5d1b4f641d81de4d5e2
Steve <80481912+pressr-inl@users.noreply.github.com>
2026-06-09 14:11:31 -0600

    Fixed tests missed by previous pull requests (#597)

--- SimulationDAL/Events.cs
+++ SimulationDAL/Events.cs
@@ -1437,6 +1437,15 @@ namespace SimulationDAL
         throw new Exception("No \"dfltTimeRate\" defined ");
       }
 
+      try //may not exist in earlier versions so use a default
+      {
+        onVarChange = (EnOnChangeTask)Enum.Parse(typeof(EnOnChangeTask), (string)dynObj.onVarChange, true);
+      }
+      catch
+      {
+        onVarChange = EnOnChangeTask.ocIgnore;
+      }
+
       try
       {
         //make sure the default values are set
@@ -1474,27 +1483,6 @@ namespace SimulationDAL
             this.AddRelatedItem(v.id);
         }
       }
-
-      if (_relatedIDs.Count > 0)
-      {
-        try
-        {
-          dynamic dynObj = (dynamic)obj;
-          if (wrapped)
-          {
-            if (dynObj.Event == null)
-              return false;
-
-            dynObj = ((dynamic)obj).Event;
-          }
-          onVarChange = (EnOnChangeTask)Enum.Parse(typeof(EnOnChangeTask), (string)dynObj.onVarChange, true);
-        }
-        catch
-        {
-          throw new Exception("parameter onVarChange missing and variables are used.");
-        }
-      }
-      
 
       return true;
     }
